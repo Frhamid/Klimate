4ffe9dc679fbf8f794107a85b8d863b5f252cc4f
Frhamid <frhamid2000@gmail.com>
2025-10-02 05:29:58 -0700

    getting coordinates of user through navigator

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,30 @@ import Layout from "./components/Layout";
 import { ThemeProvider } from "./context/theme-provider";
 import DashboardWeather from "./pages/dashboard-weather";
 import CityWeather from "./pages/city-weather";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<DashboardWeather />}></Route>
-            <Route
-              path="/cityWeather/:cityName"
-              element={<CityWeather />}
-            ></Route>
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<DashboardWeather />}></Route>
+              <Route
+                path="/cityWeather/:cityName"
+                element={<CityWeather />}
+              ></Route>
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- src/pages/dashboard-weather.tsx
+++ src/pages/dashboard-weather.tsx
@@ -1,7 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { useGeolocation } from "@/hooks/use-geolocation";
+import { RefreshCw } from "lucide-react";
 import React from "react";
 
 const DashboardWeather = () => {
-  return <div>Dashboard page</div>;
+  const { coordinates, isLoading, error, getLocation } = useGeolocation();
+  console.log("coordinates", coordinates);
+
+  const handleRefresh = () => {
+    if (coordinates) {
+      //reload weather data
+    }
+  };
+
+  return (
+    <div className="space-y-4">
+      {/* Favourite cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          className="cursor-pointer"
+          onClick={handleRefresh}
+          //   disabled={}
+        >
+          <RefreshCw className="h-4 w-4" />
+        </Button>
+      </div>
+
+      {/* current and hourly weather */}
+    </div>
+  );
 };
 
 export default DashboardWeather;
